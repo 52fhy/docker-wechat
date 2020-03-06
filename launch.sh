4358d8b3f275d2f45369c523133aa359e8512dbb
Alice_space <lizhihao0525@gmail.com>
2020-03-06 13:59:56 +0800

    fix launch

--- launch.sh
+++ launch.sh
@@ -1,4 +1,6 @@
-    docker run -d --name wechat --device /dev/snd \
+docker stop wechat &&
+docker container rm wechat &&
+docker run -d --name wechat --device /dev/snd \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $HOME/WeChatFiles:/WeChatFiles \
     -e DISPLAY=unix$DISPLAY \
